c49d388beb274ddc3be8fe0f012c60daa61418e5
GUNBOATs <gunboats@protonmail.com>
2023-05-06 19:25:40 +0700

    sortable table

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,6 +1,56 @@
-import Columns from "./Columns";
+import { useEffect, useState } from "react"
+import Image from "next/image";
+import { ethers } from 'ethers'
+import { usePerpsV2MarketDataAllProxiedMarketSummaries } from "../generated";
 
+type Coin = {
+    id: string,
+    symbol: string,
+    name: string,
+    image: string,
+    market_cap: number,
+    total_volume: number
+}
 export default function Table() {
+    const [Data, setData] = useState<Array<Coin>>();
+    const { data: listed, isLoading } = usePerpsV2MarketDataAllProxiedMarketSummaries({
+        select: (data) =>
+            data.map(
+                (market) =>
+                    ethers.utils.toUtf8String(market.asset).replace(/\0|s/g, '').toLowerCase()
+            )
+    });
+    const stablecoin = ['usdc', 'usdt', 'susd', 'dai', 'frax', 'lusd', 'busd', 'tusd', 'alusd', 'usdd', 'usdp', 'gusd'];
+    let forex = new Map<string, string>();
+    function genForex(assets: Array<string>, type: string) {
+        assets.forEach((asset) => forex.set(asset, type));
+    };
+    genForex(['xaut', 'paxg'], 'xau');
+    genForex(['eurs', 'euroc', 'ageur', 'eurt'], 'eur')
+    function listOrNot(symbol: string) {
+        if (symbol.includes('snx')) return '⛔';
+        else if (forex.has(symbol)) return '✅*';
+        else if (listed?.includes(symbol)) return '✅';
+        else return '❌';
+    }
+    useEffect(() => {
+        async function getTopCoin() {
+            const top = await fetch(
+                '/api/getcoin'
+            ).then(res => res.json());
+            console.log(top);
+            setData(top);
+        }
+        getTopCoin();
+    }, []);
+    function sortByMarketCap() {
+        const sorted = [...Data].sort((a,b) => b.market_cap - a.market_cap);
+        setData(sorted);
+    }
+    function sortByVolume() {
+        const sorted = [...Data].sort((a,b) => b.total_volume - a.total_volume);
+        setData(sorted);
+    }
     return (
         <table>
             <thead>
@@ -8,14 +58,52 @@ export default function Table() {
                     <th></th>
                     <th>Symbol</th>
                     <th>Asset</th>
-                    <th>Market Cap</th>
-                    <th>Volume</th>
+                    <th onClick={() => sortByMarketCap()}>Market Cap</th>
+                    <th onClick={() => sortByVolume()}>Volume</th>
                     <th>Listed On Synthetix Perps</th>
                 </tr>
             </thead>
             <tbody>
-                <Columns />
+                {Data && Data.filter(
+                    (coin) => {
+                        if (stablecoin.includes(coin.symbol)) return false; //stablecoin
+                        if (coin.symbol.includes('eth') && coin.symbol.length > 3) return false; // staked eth
+                        if (coin.symbol.includes('btc') && coin.symbol.length > 3) return false; // wrapped btc
+                        if (coin.symbol.startsWith('c')) return false; // compound token
+                        return true;
+                    }
+                ).map((coin) => <Column key={coin.id} coin={coin} listStatus={listOrNot(coin.symbol)} />)}
             </tbody>
         </table>
     )
+}
+
+function Column({ coin, listStatus }: { coin: Coin, listStatus: '⛔' | '✅*' | '✅' | '❌' }) {
+    return (<>
+        <tr>
+            <td>
+                <Image
+                    alt={
+                        'image for' + coin.symbol
+                    }
+                    src={coin.image}
+                    width={25}
+                    height={25} />
+            </td>
+            <td>
+                {coin.symbol.toUpperCase()}
+            </td>
+            <td>
+                {coin.name}
+            </td>
+            <td>
+                {coin.market_cap.toLocaleString()}
+            </td>
+            <td>
+                {coin.total_volume.toLocaleString()}
+            </td>
+            <td>
+                {listStatus}
+            </td>
+        </tr></>);
 }
